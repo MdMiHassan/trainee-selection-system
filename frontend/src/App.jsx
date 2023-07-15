e9907e8d6de115e7d7456ad38ad9c9cb7e49edae
Md. Mehedi Hassan <md.mehedihassan@outlook.com>
2023-07-15 20:17:40 +0600

    designed admin page nav and sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,23 @@ import AppHeader from "./layouts/AppHeader";
 import AppHero from "./layouts/AppHero";
 import ApplicantActivity from "./layouts/ApplicantActivity";
 import AppFooter from "./layouts/AppFooter";
+import AdminPanel from "./pages/AdminPanel";
+import AdminNavbar from "./layouts/AdminNavbar";
 
 const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <Layout style={{minHeight:'100vh',justifyContent:'space-between'}}>
-      <AppHeader />
-      <AppHero />
-      <ApplicantActivity />
-      <AppFooter />
-    </Layout>
+    // <Layout style={{minHeight:'100vh',justifyContent:'space-between'}}>
+    //   <AppHeader />
+    //   <AppHero />
+    //   <ApplicantActivity />
+    //   <AppFooter />
+    // </Layout>
+    <>
+    {/* <AdminNavbar /> */}
+    <AdminPanel />
+    </>
   );
 }
 
